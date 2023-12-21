37defe30286930f9877b36b4009870e265f0f6b5
Pavel Maslov <maslovpavel333@gmail.com>
2023-12-21 21:15:19 +0300

    сделал при создание загрузку множества фотографий

--- src/pages/CreateAudience/index.jsx
+++ src/pages/CreateAudience/index.jsx
@@ -3,32 +3,39 @@ import { Header} from "components";
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { Button, Text } from 'components';
-const CreateAudience = () => {
-  const [name, setName] = useState('');
-  const [audience, setAudience] = useState('');
+import { Button, Text, Img } from 'components';
+import Carousel from "react-multi-carousel";
 
-  const [image, setImage] = useState(null); // Состояние для хранения изображения
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1
+  },
+};
+
+const CreateAudience = () => {
+
+  const [images, setImages] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
   const fileInputRef = useRef(null);
   const handleImageChange = (e) => {
     if (e.target.files && e.target.files[0]) {
       let img = e.target.files[0];
-      setImage(URL.createObjectURL(img)); // Создаем URL изображения
+      setImages([...images, URL.createObjectURL(img)]);
     }
   };
+
+  const afterChangeHandler = (previousSlide, { currentSlide }) => {
+    console.log(currentSlide);
+    setActiveIndex(images[currentSlide - 2]);
+  };
+
   const handleButtonClick = () => {
-    fileInputRef.current.click(); // Программно вызывает клик по input
+    fileInputRef.current.click(); 
   };
   const handleDelete = () => {
-    setImage(null); // Программно вызывает клик по input
+    setImages(images.length === 1 ? [] : images.filter(x => x !== activeIndex));
   };
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  }
-
-  const handleAudienceChange = (e) => {
-    setAudience(e.target.value);
-  }
 
   return (
     <>
@@ -132,8 +139,31 @@ const CreateAudience = () => {
                     </div>
                   </div>
                   <div className="py-[40px] pl-[40px]">
-                    <div className="h-[350px] w-[450px] rounded-[20px]" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center', border: "1px solid gray" }}>
-                      {!image && <div className="h-full w-full bg-gray-400 rounded-[20px]"></div>} {/* Показывать серый фон, если изображение не выбрано */}
+                    <div className="h-[350px] w-[450px] rounded-[20px]" style={{backgroundSize: 'cover', backgroundPosition: 'center', border: "1px solid gray" }}>
+                      {images.length !== 0 
+                      ? <Carousel swipeable={true}
+                          draggable={true}
+                          showDots={true}
+                          responsive={responsive}
+                          ssr={true} // Server-side rendering
+                          infinite={true}
+                          transitionDuration={500}
+                          afterChange={afterChangeHandler}
+                          containerClass="pb-[30px]"
+                          removeArrowOnDeviceType={["desktop", "mobile"]}
+                          dotListClass="custom-dot-list-style"
+                          itemClass="rounded-[20px]" 
+                          className="w-full items-start" >
+                          {
+                              images.map((img) => {
+                                return(<Img
+                                  src={img} 
+                                  className="h-[350px] rounded-[20px] mx-auto">
+                              </Img>)
+                              })
+                          }
+                        </Carousel>
+                      : <div className="h-full w-full bg-gray-400 rounded-[20px]"></div>} {/* Показывать серый фон, если изображение не выбрано */}
                     </div>
                     <div className="flex flex-col items-center mt-[40px] gap-5">
                       <div className="flex flex-row items-center gap-5">
@@ -147,14 +177,11 @@ const CreateAudience = () => {
                       <Button className=" font-semibold leading-[normal] min-w-[200px] h-[60px] text-center text-l p-[0px]" onClick={handleButtonClick}>
                         Загрузить фото
                       </Button>
-                        <Button className=" font-semibold leading-[normal] min-w-[200px] h-[60px] text-center text-l p-[0px]" onClick={handleButtonClick}>
-                          Изменить фото
-                        </Button>
-
-                      </div>
                       <Button className=" font-semibold leading-[normal] min-w-[200px] h-[60px] text-center text-l p-[0px]" onClick={handleDelete}>
                         Удалить фото
                       </Button>
+
+                      </div>
                     </div>
                   </div>
 
